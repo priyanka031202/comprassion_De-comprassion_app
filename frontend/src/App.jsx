e85670fb67428eeb073c501da1d84664422c227d
Priyanka Choudhary <priyankachoudhary121405@gmail.com>
2025-06-25 01:01:10 +0530

    Update background and box colors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -49,8 +49,10 @@ export default function App() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <div className="max-w-xl mx-auto bg-white p-6 rounded-lg shadow">
+    <div  className="min-h-screen bg-gray-100 p-6">
+
+      <div className="max-w-xl mx-auto bg-blue-200 p-6 rounded-lg shadow">
+
         <h1 className="text-2xl font-bold mb-4 text-center">
           File {mode === "compress" ? "Compression" : "Decompression"} App
         </h1>
